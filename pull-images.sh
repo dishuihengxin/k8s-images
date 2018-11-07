534d19d18087e0152a571ecc1028f69ac2158518
kalid <dishuihengxin@gmail.com>
2018-11-07 18:05:19 +0800

    change dashboard dockerfile

--- pull-images.sh
+++ pull-images.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-list="kube-apiserver:v1.12.2 kube-proxy:v1.12.2 kube-scheduler:v1.12.2 kube-controller-manager:v1.12.2 coredns:1.2.2 etcd:3.2.24 pause:3.1"
+list="kube-apiserver:v1.12.2 kube-proxy:v1.12.2 kube-scheduler:v1.12.2 kube-controller-manager:v1.12.2 coredns:1.2.2 etcd:3.2.24 pause:3.1 kubernetes-dashboard-amd64:v1.10.0"
 for i in $list
 do
 	docker pull kalid/$i
